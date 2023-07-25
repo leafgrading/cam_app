2895e228ad97290536a6a9eb7bc1872d1b38a62f
leafgrading.bat <leafgrading.bat@gmail.com>
2023-07-25 18:33:52 +0600

    ui update

--- cam_app.py
+++ cam_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import math
-#from streamlit_back_camera_input import back_camera_input
+# from streamlit_back_camera_input import back_camera_input
 
 def show_image_with_text(image, text=None):
     st.markdown('<div class="image-row">', unsafe_allow_html=True)
@@ -18,7 +18,13 @@ def show_image_with_text(image, text=None):
 
 def show_five_images(uploaded_images, text=None):
     num_columns = 3
-
+    st.write('''<style>
+    [data-testid="column"] {
+        width: calc(33.3333% - 1rem) !important;
+        flex: 1 1 calc(33.3333% - 1rem) !important;
+        min-width: calc(33% - 1rem) !important;
+    }
+    </style>''', unsafe_allow_html=True)
     # Calculate the width of each column
     column_width = math.floor(12 / num_columns)
     for i, image in enumerate(uploaded_images):
@@ -35,7 +41,13 @@ def show_five_images(uploaded_images, text=None):
 
 def show_five_images2(uploaded_images, text=None):
     num_columns = 5
-
+    st.write('''<style>
+    [data-testid="column"] {
+        width: calc(33.3333% - 1rem) !important;
+        flex: 1 1 calc(33.3333% - 1rem) !important;
+        min-width: calc(33% - 1rem) !important;
+    }
+    </style>''', unsafe_allow_html=True)
     # Calculate the width of each column
     col_width = st.columns(num_columns)
     for i, image in enumerate(uploaded_images):
@@ -55,28 +67,35 @@ def change_state():
     if st.session_state.cap_count == 10:
         st.session_state.cam_state = True
 
+def image_to_base64(image):
+    from io import BytesIO
+    import base64
+    img_buffer = BytesIO()
+    image.save(img_buffer, format="PNG")
+    img_buffer.seek(0)
+    base64_image = base64.b64encode(img_buffer.read()).decode()
+    return base64_image
+
+@st.cache_resource()
+def header_view(_image):
+    st.markdown(f"""
+    <nav class="navbar navbar-primary d-flex justify-content-center" style="background-color: #103172">
+      <div class="">
+        <img src="data:image/png;base64,{image_to_base64(_image)}" alt="" width="150" height="50" />
+      </div>
+    </nav>
+    """,unsafe_allow_html=True)
+
 def main():
+    st.markdown(f'''<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">''',unsafe_allow_html=True)
     # st.title("Camera input demo")
+    # st.markdown()
     with open("custom.css") as css:
         st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
     # with open("header.html") as head:
     #     st.markdown(f"<html>{head.read()}</html>", unsafe_allow_html=True)
-    # st.write(st.session_state)
-    # Add a custom CSS style to set the background color of the header to white
-    col1, col2, col3 = st.columns([1, 3, 1])
-
-    # Logo on the left side
-    with col1:
-        st.image("BAT_Bangladesh_Logo.png", width=100)  # Replace "left_logo.png" with the path to your left logo image
-
-    # Main heading in the center
-    with col2:
-        st.title("Leaf Grading")
-
-    # Logo on the right side
-    with col3:
-        st.image("RedDot-Logo.png", width=100)  # Replace "right_logo.png" with the path to your right logo image
-
+    header_img = Image.open("./images/bat.png")
+    header_view(header_img)
 
     capture_images = []
     if "cam_state" not in st.session_state:
@@ -115,27 +134,38 @@ def main():
                     st.session_state.images = capture_images
         # st.write(len(st.session_state.images))
         st.subheader("Original Image Preview")
-        show_five_images2(st.session_state.images,None)
+        show_five_images(st.session_state.images,None)
         st.markdown("___")
         if len(st.session_state.images) >= 5:
             st.subheader("Preview (background removed)")
             val_result = ["Accepted(72%)","Accepted(72%)","Rejected","Accepted(72%)","Rejected"]
-            show_five_images2(st.session_state.images,None)
+            show_five_images(st.session_state.images,None)
             st.markdown("___")
             # st.subheader("Grading")
+            m = st.markdown("""
+                        <style>
+                        div.stButton > button:first-child {
+                            background-color: #289614;
+                            color: white;
+                            width: 300px;
+                            padding-left: 30px;
+                            padding-right: 30px;
+                        }
+                        </style>""", 
+                        unsafe_allow_html=True)
             button1 = st.button('Validate')
             if st.session_state.get('button') != True:
                 st.session_state['button'] = button1
             if st.session_state['button'] == True:
                 st.subheader("Validation Result")
-                show_five_images2(st.session_state.images,val_result)
+                show_five_images(st.session_state.images,val_result)
                 print("Validation Print Hobe")
                 st.markdown("___")
                 pred_button_pressed = st.button("Predict")
                 predicted_result = ["MDA", "MDA", "POA", "MDA", "MOA"]
                 if pred_button_pressed:
                     st.subheader("Prediction Result")
-                    show_five_images2(st.session_state.images[:3], predicted_result[:3])
+                    show_five_images(st.session_state.images[:3], predicted_result[:3])
                     st.markdown("___")
                     st.subheader("Overall Grade")
                     st.info("MDA")
